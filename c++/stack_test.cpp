aea59e3b0ec1e3d8e347e4478e33e66689c56aa2
mahaoyuan <410463229@qq.com>
2021-03-01 20:15:49 +0800

    add stack implement by list.

--- c++/stack_test.cpp
+++ c++/stack_test.cpp
@@ -10,4 +10,19 @@ int main(){
     s.Pop();
     std::cout << "Peek:" << s.Peek() << std::endl;
     s.Show();
+
+    {
+    algo::StackList<int> sl = algo::StackList<int>();
+    for (int i = 0;i < 9; i++){
+    sl.Push(i);
+    //s.Show();
+    }
+    int item = sl.Pop();
+    std::cout << "item pop:" << item << std::endl;
+    int itempeek = sl.Peek();
+    sl.Show();
+    std::cout << "item peek:" << itempeek << std::endl;
+    }
+
+
 }
